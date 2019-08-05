eb3b58ab4feb5a3da9e82b13ede41af22cf6e756
Emily Bache <emily.bache@praqma.com>
2019-08-05 17:34:45 +0200

    first draft of python version

---
+++ python/receipt_printer.py
@@ -0,0 +1,58 @@
+from model_objects import ProductUnit
+
+
+def whitespace(whitespace_size):
+    space = ''
+    for i in range(whitespace_size):
+        space += ' '
+
+    return space
+
+
+def present_quantity(item):
+    if ProductUnit.EACH == item.product.unit:
+        return str(item.quantity)
+    else:
+        return '%.3f' % item.quantity
+
+
+class ReceiptPrinter:
+
+    def __init__(self, columns=40):
+        self.columns = columns
+  
+    def print_receipt(self, receipt):
+        result = ""
+        for item in receipt.items:
+            price = "%.2f" % item.total_price
+            quantity = present_quantity(item)
+            name = item.product.name
+            unit_price = "%.2f" % item.price
+
+            whitespace_size = self.columns - name.size - price.size
+            line = name + whitespace(whitespace_size) + price + "\n"
+
+            if item.quantity != 1:
+                line += "  " + unit_price + " * " + quantity + "\n"
+
+            result += line
+
+        for discount in receipt.discounts:
+            product_presentation = discount.product.name
+            price_presentation = "%.2f" % discount.discount_amount
+            description = discount.description
+            result += description
+            result += "("
+            result += product_presentation
+            result += ")"
+            result += whitespace(self.columns - 3 - product_presentation.size - description.size - price_presentation.size)
+            result += "-"
+            result += price_presentation
+            result += "\n"
+
+        result += "\n"
+        price_presentation = "%.2f" % receipt.total_price
+        total = "Total: "
+        space = whitespace(self.columns - total.size - price_presentation.size)
+        result += total, space, price_presentation
+        return str(result)
