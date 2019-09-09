daf3d911c82f68a5cc889e5d4d676be29e048a8c
Emily Bache <emily.bache@praqma.com>
2019-09-09 13:20:46 +0200

    add asserts to python version so it matches java version

--- python/tests/test_supermarket.py
+++ python/tests/test_supermarket.py
@@ -14,12 +14,19 @@ def test_ten_percent_discount():
     apples = Product("apples", ProductUnit.KILO)
     catalog.add_product(apples, 1.99)
 
-    cart = ShoppingCart()
-    cart.add_item_quantity(apples, 2.5)
-
     teller = Teller(catalog)
     teller.add_special_offer(SpecialOfferType.TEN_PERCENT_DISCOUNT, toothbrush, 10.0)
+
+    cart = ShoppingCart()
+    cart.add_item_quantity(apples, 2.5)
 
     receipt = teller.checks_out_articles_from(cart)
 
     assert 4.975 == pytest.approx(receipt.total_price(), 0.01)
+    assert [] == receipt.discounts
+    assert 1 == len(receipt.items)
+    receipt_item = receipt.items[0]
+    assert apples == receipt_item.product
+    assert 1.99 == receipt_item.price
+    assert 2.5 * 1.99 == pytest.approx(receipt_item.total_price, 0.01)
+    assert 2.5 == receipt_item.quantity
