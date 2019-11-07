233fee6b156b170cc912f8027aff89d2285d2a07
Emily Bache <emily.bache@praqma.com>
2019-11-07 13:09:47 +0100

    refactored receipt printer to make it more readable and less duplication

--- java/src/main/java/dojo/supermarket/ReceiptPrinter.java
+++ java/src/main/java/dojo/supermarket/ReceiptPrinter.java
@@ -19,38 +19,58 @@ public class ReceiptPrinter {
     public String printReceipt(Receipt receipt) {
         StringBuilder result = new StringBuilder();
         for (ReceiptItem item : receipt.getItems()) {
-            String price = String.format(Locale.UK, "%.2f", item.getTotalPrice());
-            String quantity = presentQuantity(item);
-            String name = item.getProduct().getName();
-            String unitPrice = String.format(Locale.UK, "%.2f", item.getPrice());
-
-            int whitespaceSize = this.columns - name.length() - price.length();
-            String line = name + getWhitespace(whitespaceSize) + price + "\n";
-
-            if (item.getQuantity() != 1) {
-                line += "  " + unitPrice + " * " + quantity + "\n";
-            }
-            result.append(line);
+            String receiptItem = presentReceiptItem(item);
+            result.append(receiptItem);
         }
         for (Discount discount : receipt.getDiscounts()) {
-            String productPresentation = discount.getProduct().getName();
-            String pricePresentation = String.format(Locale.UK, "%.2f", discount.getDiscountAmount());
-            String description = discount.getDescription();
-            result.append(description);
-            result.append("(");
-            result.append(productPresentation);
-            result.append(")");
-            result.append(getWhitespace(this.columns - 3 - productPresentation.length() - description.length() - pricePresentation.length()));
-            result.append("-");
-            result.append(pricePresentation);
-            result.append("\n");
+            String discountPresentation = presentDiscount(discount);
+            result.append(discountPresentation);
         }
+
         result.append("\n");
-        String pricePresentation = String.format(Locale.UK, "%.2f", (double) receipt.getTotalPrice());
-        String total = "Total: ";
-        String whitespace = getWhitespace(this.columns - total.length() - pricePresentation.length());
-        result.append(total).append(whitespace).append(pricePresentation);
+        result.append(presentTotal(receipt));
         return result.toString();
+    }
+
+    private String presentReceiptItem(ReceiptItem item) {
+        String totalPricePresentation = presentPrice(item.getTotalPrice());
+        String name = item.getProduct().getName();
+
+        String line = formatLineWithWhitespace(name, totalPricePresentation);
+
+        if (item.getQuantity() != 1) {
+            line += "  " + presentPrice(item.getPrice()) + " * " + presentQuantity(item) + "\n";
+        }
+        return line;
+    }
+
+    private String presentDiscount(Discount discount) {
+        String name = discount.getDescription() + "(" + discount.getProduct().getName() + ")";
+        String value = "-" + presentPrice(discount.getDiscountAmount());
+
+        return formatLineWithWhitespace(name, value);
+    }
+
+    private String presentTotal(Receipt receipt) {
+        String name = "Total: ";
+        String value = presentPrice(receipt.getTotalPrice());
+        return formatLineWithWhitespace(name, value);
+    }
+
+    private String formatLineWithWhitespace(String name, String value) {
+        StringBuilder line = new StringBuilder();
+        line.append(name);
+        int whitespaceSize = this.columns - name.length() - value.length();
+        for (int i = 0; i < whitespaceSize; i++) {
+            line.append(" ");
+        }
+        line.append(value);
+        line.append('\n');
+        return line.toString();
+    }
+
+    private static String presentPrice(double price) {
+        return String.format(Locale.UK, "%.2f", price);
     }
 
     private static String presentQuantity(ReceiptItem item) {
@@ -59,11 +79,4 @@ public class ReceiptPrinter {
                 : String.format(Locale.UK, "%.3f", item.getQuantity());
     }
 
-    private static String getWhitespace(int whitespaceSize) {
-        StringBuilder whitespace = new StringBuilder();
-        for (int i = 0; i < whitespaceSize; i++) {
-            whitespace.append(" ");
-        }
-        return whitespace.toString();
-    }
 }
