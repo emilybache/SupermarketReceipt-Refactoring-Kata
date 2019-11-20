3988ed5ba20769fdd78ff34f536439b0bcf03130
Emily Bache <emily.bache@praqma.com>
2019-11-20 15:24:49 +0100

    tweak starting position to make it slightly more readable

--- cpp/test/SuperMarketTest.cpp
+++ cpp/test/SuperMarketTest.cpp
@@ -15,18 +15,16 @@ TEST_CASE("Discounts", "[Supermarket]")
     catalog->addProduct(toothbrush, 0.99);
     Product apples("apples", ProductUnit::Kilo);
     catalog->addProduct(apples, 1.99);
+    Teller teller(catalog);
+    teller.addSpecialOffer(SpecialOfferType::TenPercentDiscount, toothbrush, 10.0);
 
     ShoppingCart cart;
     cart.addItemQuantity(apples, 2.5);
 
-    Teller teller(catalog);
-    teller.addSpecialOffer(SpecialOfferType::TenPercentDiscount, toothbrush, 10.0);
-
     Receipt receipt = teller.checksOutArticlesFrom(cart);
 
     REQUIRE(4.975 == receipt.getTotalPrice());
-
-    REQUIRE(0 == receipt.getDiscounts().size());
+    REQUIRE(receipt.getDiscounts().empty());
     REQUIRE(1 == receipt.getItems().size());
     ReceiptItem receiptItem = receipt.getItems()[0];
     REQUIRE(apples == receiptItem.getProduct());
