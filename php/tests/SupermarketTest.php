11e6a36c7de0e342df8258320353655db517f4d7
Hossein Taleghani <hossein.t@appson.ir>
2020-05-24 04:33:31 +0430

    add supermarket approval tests

--- php/tests/SupermarketTest.php
+++ php/tests/SupermarketTest.php
@@ -2,38 +2,189 @@
 
 namespace Tests;
 
+use ApprovalTests\Approvals;
 use PHPUnit\Framework\TestCase;
-use Supermarket\Model\{Teller, Product, ProductUnit, ShoppingCart, SpecialOfferType};
+use Supermarket\Model\{Receipt, SpecialOfferType, Teller, Product, ProductUnit, ShoppingCart};
+use Supermarket\ReceiptPrinter;
 
 class SupermarketTest extends TestCase
 {
-    public function testTenPercentDiscount()
+    private FakeCatalog $catalog;
+    private Product $toothbrush;
+    private Product $rice;
+    private Product $apples;
+    private Product $cherryTomatoes;
+    private ShoppingCart $cart;
+    private Teller $teller;
+    private ReceiptPrinter $printer;
+
+    public function setUp(): void
     {
-        // Arrange
-        $catalog = new FakeCatalog();
-        $toothbrush = new Product('toothbrush', ProductUnit::each());
-        $catalog->addProduct($toothbrush, 0.99);
-        $apples = new Product('apples', ProductUnit::kilo());
-        $catalog->addProduct($apples, 1.99);
+        parent::setUp();
 
-        $cart = new ShoppingCart();
-        $cart->addItemQuantity($apples, 2.5);
+        $this->catalog = new FakeCatalog();
+        $this->cart = new ShoppingCart();
+        $this->teller = new Teller($this->catalog);
 
-        $teller = new Teller($catalog);
-        $teller->addSpecialOffer(SpecialOfferType::tenPercentDiscount(), $toothbrush, 10.0);
+        $this->toothbrush = new Product('toothbrush', ProductUnit::each());
+        $this->catalog->addProduct($this->toothbrush, 0.99);
+        $this->rice = new Product('rice', ProductUnit::each());
+        $this->catalog->addProduct($this->rice, 2.99);
+        $this->apples = new Product('apples', ProductUnit::kilo());
+        $this->catalog->addProduct($this->apples, 1.99);
+        $this->cherryTomatoes = new Product('cherry tomato box', ProductUnit::each());
+        $this->catalog->addProduct($this->cherryTomatoes, 0.69);
 
-        // Act
-        $receipt = $teller->checkoutArticlesFrom($cart);
+        $this->printer = new ReceiptPrinter(40);
+    }
 
-        // Assert
-        self::assertEquals(4.975, $receipt->getTotalPrice());
-        self::assertEquals([], $receipt->getDiscounts());
-        self::assertCount(1, $receipt->getItems());
-        $receiptItem = $receipt->getItems()[0];
-        self::assertEquals($apples, $receiptItem->getProduct());
-        self::assertEquals(1.99, $receiptItem->getPrice());
-        self::assertEquals(2.5 * 1.99, $receiptItem->getTotalPrice());
-        self::assertEquals(2.5, $receiptItem->getQuantity());
+    /** @test */
+    public function anEmptyShoppingCartShouldCostNothing()
+    {
+        Approvals::verifyString($this->printReceipt());
+    }
+
+    /** @test */
+    public function oneNormalItem()
+    {
+        $this->cart->addItem($this->toothbrush);
+
+        Approvals::verifyString($this->printReceipt());
+    }
+
+    /** @test */
+    public function twoNormalItems()
+    {
+        $this->cart->addItem($this->toothbrush);
+        $this->cart->addItem($this->rice);
+
+        Approvals::verifyString($this->printReceipt());
+    }
+
+    /** @test */
+    public function buyTwoGetOneFree()
+    {
+        $this->cart->addItem($this->toothbrush);
+        $this->cart->addItem($this->toothbrush);
+        $this->cart->addItem($this->toothbrush);
+        $this->teller->addSpecialOffer(
+            SpecialOfferType::threeForTwo(),
+            $this->toothbrush,
+            $this->catalog->getUnitPrice($this->toothbrush)
+        );
+
+        Approvals::verifyString($this->printReceipt());
+    }
+
+    /** @test */
+    public function buyTwoGetOneFreeButInsufficientInBasket()
+    {
+        $this->cart->addItem($this->toothbrush);
+        $this->teller->addSpecialOffer(
+            SpecialOfferType::threeForTwo(),
+            $this->toothbrush,
+            $this->catalog->getUnitPrice($this->toothbrush)
+        );
+
+        Approvals::verifyString($this->printReceipt());
+    }
+
+    /** @test */
+    public function buyFiveGetOneFree()
+    {
+        $this->cart->addItem($this->toothbrush);
+        $this->cart->addItem($this->toothbrush);
+        $this->cart->addItem($this->toothbrush);
+        $this->cart->addItem($this->toothbrush);
+        $this->cart->addItem($this->toothbrush);
+        $this->teller->addSpecialOffer(
+            SpecialOfferType::threeForTwo(),
+            $this->toothbrush,
+            $this->catalog->getUnitPrice($this->toothbrush)
+        );
+
+        Approvals::verifyString($this->printReceipt());
+    }
+
+    /** @test */
+    public function looseWeightProduct()
+    {
+        $this->cart->addItemQuantity($this->apples, 0.5);
+
+        Approvals::verifyString($this->printReceipt());
+    }
+
+    /** @test */
+    public function percentDiscount()
+    {
+        $this->cart->addItem($this->rice);
+        $this->teller->addSpecialOffer(SpecialOfferType::tenPercentDiscount(), $this->rice, 10.0);
+
+        Approvals::verifyString($this->printReceipt());
+    }
+
+    /** @test */
+    public function xForYDiscount()
+    {
+        $this->cart->addItem($this->cherryTomatoes);
+        $this->cart->addItem($this->cherryTomatoes);
+        $this->teller->addSpecialOffer(SpecialOfferType::twoForAmount(), $this->cherryTomatoes, 0.99);
+
+        Approvals::verifyString($this->printReceipt());
+    }
+
+    /** @test */
+    public function xForYDiscountWithInsufficientInBasket()
+    {
+        $this->cart->addItem($this->cherryTomatoes);
+        $this->teller->addSpecialOffer(SpecialOfferType::twoForAmount(), $this->cherryTomatoes, 0.99);
+
+        Approvals::verifyString($this->printReceipt());
+    }
+
+    /** @test */
+    public function fiveForYDiscount()
+    {
+        $this->cart->addItemQuantity($this->apples, 5);
+        $this->teller->addSpecialOffer(SpecialOfferType::fiveForAmount(), $this->apples, 6.99);
+
+        Approvals::verifyString($this->printReceipt());
+    }
+
+    /** @test */
+    public function fiveForYDiscountWithSix()
+    {
+        $this->cart->addItemQuantity($this->apples, 6);
+        $this->teller->addSpecialOffer(SpecialOfferType::fiveForAmount(), $this->apples, 6.99);
+
+        Approvals::verifyString($this->printReceipt());
+    }
+
+    /** @test */
+    public function fiveForYDiscountWithSixteen()
+    {
+        $this->cart->addItemQuantity($this->apples, 16);
+        $this->teller->addSpecialOffer(SpecialOfferType::fiveForAmount(), $this->apples, 6.99);
+
+        Approvals::verifyString($this->printReceipt());
+    }
+
+    /** @test */
+    public function fiveForYDiscountWithFour()
+    {
+        $this->cart->addItemQuantity($this->apples, 4);
+        $this->teller->addSpecialOffer(SpecialOfferType::fiveForAmount(), $this->apples, 6.99);
+
+        Approvals::verifyString($this->printReceipt());
+    }
+
+    /**
+     * @param Receipt $receipt
+     * @return string
+     */
+    private function printReceipt(): string
+    {
+        $receipt = $this->teller->checkoutArticlesFrom($this->cart);
+        return $this->printer->printReceipt($receipt);
     }
 }
-
