07bb15a397c6d18c30d8e60b01703c94273b80e9
Emily Bache <emily@bacheconsulting.com>
2020-11-25 07:20:01 +0000

    make reporter configurable

--- python/tests/test_receipt_printer.py
+++ python/tests/test_receipt_printer.py
@@ -1,6 +1,7 @@
 import unittest
 
 from approvaltests import verify
+from approvaltests.reporters import ClipboardReporter
 
 from model_objects import Product, SpecialOfferType, ProductUnit, Discount
 from receipt import Receipt
@@ -10,34 +11,35 @@ from receipt_printer import ReceiptPrinter
 
 class ReceiptPrinterTest(unittest.TestCase):
     def setUp(self):
+        self.reporter = ClipboardReporter()
         self.toothbrush = Product("toothbrush", ProductUnit.EACH)
         self.apples = Product("apples", ProductUnit.KILO)
         self.receipt = Receipt()
 
     def test_one_line_item(self):
         self.receipt.add_product(self.toothbrush, 1, 0.99, 0.99)
-        verify(ReceiptPrinter().print_receipt(self.receipt))
+        verify(ReceiptPrinter().print_receipt(self.receipt), reporter=self.reporter)
 
     def test_quantity_two(self):
         self.receipt.add_product(self.toothbrush, 2, 0.99, 0.99 * 2)
-        verify(ReceiptPrinter().print_receipt(self.receipt))
+        verify(ReceiptPrinter().print_receipt(self.receipt), reporter=self.reporter)
 
     def test_loose_weight(self):
         self.receipt.add_product(self.apples, 2.3, 1.99, 1.99 * 2.3)
-        verify(ReceiptPrinter().print_receipt(self.receipt))
+        verify(ReceiptPrinter().print_receipt(self.receipt), reporter=self.reporter)
 
     def test_total(self):
         self.receipt.add_product(self.toothbrush, 1, 0.99, 0.99 * 2)
         self.receipt.add_product(self.apples, 0.75, 1.99, 1.99 * 0.75)
-        verify(ReceiptPrinter().print_receipt(self.receipt))
+        verify(ReceiptPrinter().print_receipt(self.receipt), reporter=self.reporter)
 
     def test_discounts(self):
         self.receipt.add_discount(Discount(self.apples, "3 for 2", -0.99))
-        verify(ReceiptPrinter().print_receipt(self.receipt))
+        verify(ReceiptPrinter().print_receipt(self.receipt), reporter=self.reporter)
 
     def test_whole_receipt(self):
         self.receipt.add_product(self.toothbrush, 1, 0.99, 0.99)
         self.receipt.add_product(self.toothbrush, 2, 0.99, 0.99*2)
         self.receipt.add_product(self.apples, 0.75, 1.99, 1.99 * 0.75)
         self.receipt.add_discount(Discount(self.apples, "3 for 2", -0.99))
-        verify(ReceiptPrinter().print_receipt(self.receipt))
+        verify(ReceiptPrinter().print_receipt(self.receipt), reporter=self.reporter)
