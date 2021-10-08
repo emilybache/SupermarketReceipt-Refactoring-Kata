95243fd71f64e1a2cbd15de043ebad3b7c46ab0c
emily <emily@bacheconsulting.com>
2021-10-08 17:21:18 +0200

    added catch2 test with approvals

--- c/test-catch2/supermarket_catch.cpp
+++ c/test-catch2/supermarket_catch.cpp
@@ -1,22 +1,43 @@
 #include "ApprovalTests.hpp"
 #include "catch2/catch.hpp"
+#include <string>
 
 extern "C"
 {
 #include "supermarket.h"
+#include "printer.h"
 }
 
+using namespace std;
 
 TEST_CASE ("Supermarket") {
+    struct product_t* toothbrush = product_create("toothbrush", Each);
+    struct product_t* apples = product_create("apples", Kilo);
+    struct product_t* rice = product_create("rice", Each);
+    struct product_t* cherry_tomtoes = product_create("cherry tomato box", Each);
+    double toothbrush_price = 0.99;
+    double apple_price = 1.99;
+    double rice_price = 2.99;
+    double tomato_price = 0.69;
+    struct product_t products[MAX_PRODUCTS] = {*toothbrush, *apples, *rice, *cherry_tomtoes};
+    double prices[MAX_PRODUCTS] = {toothbrush_price, apple_price, rice_price, tomato_price};
+    struct catalog_t* catalog = catalog_create(products, prices, 4);
+
+    char buffer[MAX_PRINT_LENGTH];
+
+    SECTION("Empty cart costs nothing") {
+
+        struct teller_t* teller = teller_create(catalog, 4, nullptr);
+
+        struct cart_t* cart = cart_create({}, {}, 0);
+
+        struct receipt_t* receipt = check_out_articles(teller, cart);
+
+        print_receipt(buffer, receipt);
+        ApprovalTests::Approvals::verify(string(buffer));
+    }
+
     SECTION("Ten percent discount") {
-        // ARRANGE
-        struct product_t* toothbrush = product_create("toothbrush", Each);
-        struct product_t* apples = product_create("apples", Kilo);
-        double toothbrush_price = 0.99;
-        double apple_price = 1.99;
-        struct product_t products[MAX_PRODUCTS] = {*toothbrush, *apples};
-        double prices[MAX_PRODUCTS] = {toothbrush_price, apple_price};
-        struct catalog_t* catalog = catalog_create(products, prices, 2);
         struct special_offer_t* special_offer = special_offer_create(TenPercentDiscount, toothbrush, 10.0);
 
         struct teller_t* teller = teller_create(catalog, 2, special_offer);
@@ -37,6 +58,7 @@ TEST_CASE ("Supermarket") {
         CHECK(2.5*1.99 == item.totalPrice);
         CHECK(2.5 == item.quantity);
     }
+
 }
 
 
