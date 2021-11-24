84af213f010971300fa2e7cca6455e8c08f6ce43
Jibidus <jibidus@gmail.com>
2021-11-24 09:19:29 +0100

    Add python source code

---
+++ python/receipt.py
@@ -0,0 +1,35 @@
+
+class ReceiptItem:
+    def __init__(self, product, quantity, price, total_price):
+        self.product = product
+        self.quantity = quantity
+        self.price = price
+        self.total_price = total_price
+
+
+class Receipt:
+    def __init__(self):
+        self._items = []
+        self._discounts = []
+
+    def total_price(self):
+        total = 0
+        for item in self.items:
+            total += item.total_price
+        for discount in self.discounts:
+            total += discount.discount_amount
+        return total
+
+    def add_product(self, product, quantity, price, total_price):
+        self._items.append(ReceiptItem(product, quantity, price, total_price))
+
+    def add_discount(self, discount):
+        self._discounts.append(discount)
+
+    @property
+    def items(self):
+        return self._items[:]
+
+    @property
+    def discounts(self):
+        return self._discounts[:]
