3ee51c518c5682cadd1421589e1e825cc85d4bb7
emily <emily@bacheconsulting.com>
2022-06-12 17:15:40 +0200

    Approved scrubbed version

--- csharp/SupermarketReceipt.Test/ReceiptPrinterTest.cs
+++ csharp/SupermarketReceipt.Test/ReceiptPrinterTest.cs
@@ -17,21 +17,21 @@ namespace SupermarketReceipt.Test
         public Task oneLineItem()
         {
             _receipt.AddProduct(_toothbrush, 1, 0.99, 0.99);
-            return Verifier.Verify(new ReceiptPrinter().PrintReceipt(_receipt));
+            return VerifyReceipt(_receipt);
         }
         
         [Fact]
         public Task quantityTwo()
         {
             _receipt.AddProduct(_toothbrush, 2, 0.99, 0.99 * 2);
-            return Verifier.Verify(new ReceiptPrinter().PrintReceipt(_receipt));
+            return VerifyReceipt(_receipt);
         }
         
         [Fact]
         public Task looseWeight()
         {
             _receipt.AddProduct(_apples, 2.3, 1.99, 1.99 * 2.3);
-            return Verifier.Verify(new ReceiptPrinter().PrintReceipt(_receipt));
+            return VerifyReceipt(_receipt);
         }
 
         [Fact]
@@ -40,14 +40,14 @@ namespace SupermarketReceipt.Test
 
             _receipt.AddProduct(_toothbrush, 1, 0.99, 2 * 0.99);
             _receipt.AddProduct(_apples, 0.75, 1.99, 1.99 * 0.75);
-            return Verifier.Verify(new ReceiptPrinter().PrintReceipt(_receipt));
+            return VerifyReceipt(_receipt);
         }
 
         [Fact]
         public Task discounts()
         {
             _receipt.AddDiscount(new Discount(_apples, "3 for 2", 0.99));
-            return Verifier.Verify(new ReceiptPrinter().PrintReceipt(_receipt));
+            return VerifyReceipt(_receipt);
         }
 
         [Fact]
