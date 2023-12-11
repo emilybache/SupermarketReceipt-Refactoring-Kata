aa5b6f5b011151af3c24cef83b1cbb72b7bd054f
Kerry Buckley <kerryjbuckley@gmail.com>
2023-12-11 20:57:28 +0000

    Remove generated boilerplate code

---
+++ elixir/README-elixir.md
@@ -0,0 +1,7 @@
+# README (Elixir)
+
+This is the branch with approval tests. To run them:
+
+    mix test
+
+There are no external dependencies other than Elixir itself.
--- elixir/test/supermarket_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule SupermarketTest do
-  use ExUnit.Case
-  doctest Supermarket
-
-  test "greets the world" do
-    assert Supermarket.hello() == :world
-  end
-end
