df0fccd99311d02ae50df15c0ad3069e25e90cf5
Kerry Buckley <kerryjbuckley@gmail.com>
2023-12-11 22:31:30 +0000

    Implement approval test macros, and add files with expected outputs

--- elixir/test/receipt_printer_test.exs
+++ elixir/test/receipt_printer_test.exs
@@ -1,5 +1,6 @@
 defmodule ReceiptPrinterTest do
   use ExUnit.Case, async: true
+  import Approvals
 
   alias Supermarket.Discount
   alias Supermarket.Product
@@ -12,31 +13,31 @@ defmodule ReceiptPrinterTest do
     %{receipt: %Receipt{}}
   end
 
-  test "one line item", %{receipt: receipt} do
+  approve "one line item", %{receipt: receipt} do
     receipt = Receipt.add_product(receipt, @toothbrush, 1, 0.99, 0.99)
-    Approvals.verify(ReceiptPrinter.print_receipt(receipt, 40))
+    verify(ReceiptPrinter.print_receipt(receipt, 40))
   end
 
-  test "quantity two", %{receipt: receipt} do
+  approve "quantity two", %{receipt: receipt} do
     receipt = Receipt.add_product(receipt, @toothbrush, 2, 0.99, 0.99 * 2)
-    Approvals.verify(ReceiptPrinter.print_receipt(receipt, 40))
+    verify(ReceiptPrinter.print_receipt(receipt, 40))
   end
 
-  test "loose weight", %{receipt: receipt} do
+  approve "loose weight", %{receipt: receipt} do
     receipt = Receipt.add_product(receipt, @apples, 2.3, 1.99, 1.99 * 2.3)
-    Approvals.verify(ReceiptPrinter.print_receipt(receipt, 40))
+    verify(ReceiptPrinter.print_receipt(receipt, 40))
   end
 
-  test "total", %{receipt: receipt} do
+  approve "total", %{receipt: receipt} do
     receipt =
       receipt
       |> Receipt.add_product(@toothbrush, 1, 0.99, 2 * 0.99)
       |> Receipt.add_product(@apples, 0.75, 1.99, 1.99 * 0.75)
 
-    Approvals.verify(ReceiptPrinter.print_receipt(receipt, 40))
+    verify(ReceiptPrinter.print_receipt(receipt, 40))
   end
 
-  test "discounts", %{receipt: receipt} do
+  approve "discounts", %{receipt: receipt} do
     receipt =
       Receipt.add_discount(receipt, %Discount{
         product: @apples,
@@ -44,10 +45,10 @@ defmodule ReceiptPrinterTest do
         discount_amount: -0.99
       })
 
-    Approvals.verify(ReceiptPrinter.print_receipt(receipt, 40))
+    verify(ReceiptPrinter.print_receipt(receipt, 40))
   end
 
-  test "print whole receipt", %{receipt: receipt} do
+  approve "print whole receipt", %{receipt: receipt} do
     receipt =
       receipt
       |> Receipt.add_product(@toothbrush, 1, 0.99, 0.99)
@@ -59,6 +60,6 @@ defmodule ReceiptPrinterTest do
         discount_amount: -0.99
       })
 
-    Approvals.verify(ReceiptPrinter.print_receipt(receipt, 40))
+    verify(ReceiptPrinter.print_receipt(receipt, 40))
   end
 end
--- elixir/test/support/approvals.ex
+++ elixir/test/support/approvals.ex
@@ -1,4 +1,21 @@
 defmodule Approvals do
-  def verify(_expected) do
+  defmacro approve(name, context \\ %{}, expr) do
+    quote do
+      test unquote(name), unquote(context) = %{module: approvals_module, test: approvals_test} do
+        var!(approvals_file) =
+          approvals_module
+          |> Macro.underscore()
+          |> Path.join(approvals_test |> to_string |> String.replace(~r/\W/, "_"))
+          |> Path.expand("test/support/approvals")
+
+        unquote(expr)
+      end
+    end
+  end
+
+  defmacro verify(expr) do
+    quote do
+      assert unquote(expr) == File.read!(var!(approvals_file))
+    end
   end
 end
