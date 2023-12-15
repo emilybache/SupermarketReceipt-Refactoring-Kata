1db176f5b638dc4d27832a4e435126f6303db432
Kerry Buckley <kerryjbuckley@gmail.com>
2023-12-15 21:27:22 +0000

    Configure formatter not to add parens to verify test macro calls

--- elixir/test/receipt_printer_test.exs
+++ elixir/test/receipt_printer_test.exs
@@ -15,17 +15,17 @@ defmodule ReceiptPrinterTest do
 
   approve "one line item", %{receipt: receipt} do
     receipt = Receipt.add_product(receipt, @toothbrush, 1, 0.99, 0.99)
-    verify(ReceiptPrinter.print_receipt(receipt, 40))
+    verify ReceiptPrinter.print_receipt(receipt, 40)
   end
 
   approve "quantity two", %{receipt: receipt} do
     receipt = Receipt.add_product(receipt, @toothbrush, 2, 0.99, 0.99 * 2)
-    verify(ReceiptPrinter.print_receipt(receipt, 40))
+    verify ReceiptPrinter.print_receipt(receipt, 40)
   end
 
   approve "loose weight", %{receipt: receipt} do
     receipt = Receipt.add_product(receipt, @apples, 2.3, 1.99, 1.99 * 2.3)
-    verify(ReceiptPrinter.print_receipt(receipt, 40))
+    verify ReceiptPrinter.print_receipt(receipt, 40)
   end
 
   approve "total", %{receipt: receipt} do
@@ -34,7 +34,7 @@ defmodule ReceiptPrinterTest do
       |> Receipt.add_product(@toothbrush, 1, 0.99, 2 * 0.99)
       |> Receipt.add_product(@apples, 0.75, 1.99, 1.99 * 0.75)
 
-    verify(ReceiptPrinter.print_receipt(receipt, 40))
+    verify ReceiptPrinter.print_receipt(receipt, 40)
   end
 
   approve "discounts", %{receipt: receipt} do
@@ -45,7 +45,7 @@ defmodule ReceiptPrinterTest do
         discount_amount: -0.99
       })
 
-    verify(ReceiptPrinter.print_receipt(receipt, 40))
+    verify ReceiptPrinter.print_receipt(receipt, 40)
   end
 
   approve "print whole receipt", %{receipt: receipt} do
@@ -60,6 +60,6 @@ defmodule ReceiptPrinterTest do
         discount_amount: -0.99
       })
 
-    verify(ReceiptPrinter.print_receipt(receipt, 40))
+    verify ReceiptPrinter.print_receipt(receipt, 40)
   end
 end
