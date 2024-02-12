d25cbb5707e696b4e21a5ad3671322b82c0fd8f1
barreno <lmbarreno2@outlook.com>
2024-02-12 20:15:12 +0100

    Add tests for exceptions on catalog and test for including same product on cart 2 times

--- python/tests/test_supermarket.py
+++ python/tests/test_supermarket.py
@@ -1,5 +1,7 @@
 import approvaltests
+import pytest
 
+from catalog import SupermarketCatalog
 from model_objects import SpecialOfferType
 from receipt_printer import ReceiptPrinter
 from teller import Teller
@@ -59,3 +61,32 @@ def test_five_items_for_reduced_price_discount(catalog_with_toothbrush, toothbru
 
     approvaltests.verify(string_receipt)
 
+
+def test_cart_with_same_product_no_discount(catalog_with_toothbrush, toothbrush_product, shopping_cart):
+    teller = Teller(catalog_with_toothbrush)
+    teller.add_special_offer(SpecialOfferType.FIVE_FOR_AMOUNT, toothbrush_product, 4)
+
+    shopping_cart.add_item_quantity(toothbrush_product, 1)
+    shopping_cart.add_item_quantity(toothbrush_product, 1)
+    receipt = teller.checks_out_articles_from(shopping_cart)
+    string_receipt = ReceiptPrinter().print_receipt(receipt)
+
+    approvaltests.verify(string_receipt)
+
+
+def test_add_product_exception_on_catalog(apple_product):
+    catalog = SupermarketCatalog()
+
+    with pytest.raises(Exception) as ex:
+        catalog.add_product(apple_product, 10)
+
+    assert str(ex.value) == 'cannot be called from a unit test - it accesses the database'
+
+
+def test_unit_price_exception_on_catalog(apple_product):
+    catalog = SupermarketCatalog()
+
+    with pytest.raises(Exception) as ex:
+        catalog.unit_price(apple_product)
+
+    assert str(ex.value) == 'cannot be called from a unit test - it accesses the database'
