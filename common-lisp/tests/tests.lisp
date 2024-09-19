af65a31ca07d407c834138623110e23798a99a6a
Nico Simoski <nico_simoski@mail.de>
2024-09-19 11:49:56 +0200

    Added unit tests with reasonable coverage for common lisp

--- common-lisp/tests/tests.lisp
+++ common-lisp/tests/tests.lisp
@@ -4,35 +4,179 @@
 
 (define-test supermarket-receipt-testsuite)
 
-(define-test "Ten percent discount"
+(define-test "An empty basket is for free."
   :parent supermarket-receipt-testsuite
   (let* ((a-catalog (make-instance 'fake-catalog))
          (a-toothbrush (make-instance 'product
-                         :name "toothbrush"
-                         :unit 'each))
+                                      :name "toothbrush"
+                                      :unit 'each))
+         (price-for-toothbrush 1.00)
          (apples (make-instance 'product
-                   :name "apples"
-                   :unit 'kilo))
+                                :name "apples"
+                                :unit 'kilo))
+         (price-for-apples 2.00)
+         (expected-price 0.00)
          (a-teller (make-instance 'teller
                      :catalog a-catalog))
          (a-cart (make-instance 'shopping-cart)))
-    (add-product-to-catalog a-catalog a-toothbrush 0.99)
-    (add-product-to-catalog a-catalog apples 1.99)
-    (add-special-offer a-teller 'ten-percent-discount a-toothbrush 10.0)
-    (add-item-quantity a-cart apples 2.5)
-    (let* ((a-receipt (checks-out-articles-from a-teller a-cart))
-           (total-receipt-price (total-price a-receipt))
-           (discounts (receipt-discounts a-receipt))
-           (items (receipt-items a-receipt))
-           (first-receipt-item (first items))
-           (receipt-item-product (item-product first-receipt-item))
-           (the-price (item-price first-receipt-item))
-           (the-total-price (item-total-price first-receipt-item))
-           (the-quantity (item-quantity first-receipt-item)))
-      (is = 4.975 total-receipt-price)
-      (is equal nil discounts)
-      (is = 1 (length items))
-      (is equal apples receipt-item-product)
-      (is = 1.99 the-price)
-      (is = (* 2.5 1.99) the-total-price)
-      (is = 2.5 the-quantity))))
+    (add-product-to-catalog a-catalog a-toothbrush price-for-toothbrush)
+    (add-product-to-catalog a-catalog apples price-for-apples)
+    (let* ((a-receipt (checks-out-articles-from a-teller a-cart)))
+      (is = expected-price (total-price a-receipt)))))
+
+(define-test "A ten percent discount is not granted for the wrong product."
+  :parent supermarket-receipt-testsuite
+  (let* ((a-catalog (make-instance 'fake-catalog))
+         (a-toothbrush (make-instance 'product
+                                      :name "toothbrush"
+                                      :unit 'each))
+         (price-for-toothbrush 1.00)
+         (discount-for-toothbrush 10.0)
+         (apples (make-instance 'product
+                                :name "apples"
+                                :unit 'kilo))
+         (price-for-apples 2.00)
+         (quantity-of-apples 2.5)
+         (expected-price 5.00)
+         (a-teller (make-instance 'teller
+                     :catalog a-catalog))
+         (a-cart (make-instance 'shopping-cart)))
+    (add-product-to-catalog a-catalog a-toothbrush price-for-toothbrush)
+    (add-product-to-catalog a-catalog apples price-for-apples)
+    (add-special-offer a-teller 'ten-percent-discount a-toothbrush discount-for-toothbrush)
+    (add-item-quantity a-cart apples quantity-of-apples)
+    (let* ((a-receipt (checks-out-articles-from a-teller a-cart)))
+      (is = expected-price (total-price a-receipt)))))
+
+(define-test "A ten percent discount is granted for the right product."
+  :parent supermarket-receipt-testsuite
+  (let* ((a-catalog (make-instance 'fake-catalog))
+         (a-toothbrush (make-instance 'product
+                                      :name "toothbrush"
+                                      :unit 'each))
+         (price-for-toothbrush 1.00)
+         (number-of-toothbrushes 1.0)
+         (discount-for-toothbrush 10.0)
+         (expected-price 0.90)
+         (a-teller (make-instance 'teller
+                                  :catalog a-catalog))
+         (a-cart (make-instance 'shopping-cart)))
+    (add-product-to-catalog a-catalog a-toothbrush price-for-toothbrush)
+    (add-item-quantity a-cart a-toothbrush number-of-toothbrushes)
+    (add-special-offer a-teller 'ten-percent-discount a-toothbrush discount-for-toothbrush)
+    (let* ((a-receipt (checks-out-articles-from a-teller a-cart)))
+      (is = expected-price (total-price a-receipt)))))
+
+(define-test "A three for two discount is not granted for two of the right product."
+  :parent supermarket-receipt-testsuite
+  (let* ((a-catalog (make-instance 'fake-catalog))
+         (a-toothbrush (make-instance 'product
+                                      :name "toothbrush"
+                                      :unit 'each))
+         (price-for-toothbrush 1.00)
+         (number-of-toothbrushes 2.0)
+         (discount-for-toothbrush 10.0)
+         (expected-price 2.00)
+         (a-teller (make-instance 'teller
+                                  :catalog a-catalog))
+         (a-cart (make-instance 'shopping-cart)))
+    (add-product-to-catalog a-catalog a-toothbrush price-for-toothbrush)
+    (add-item-quantity a-cart a-toothbrush number-of-toothbrushes)
+    (add-special-offer a-teller 'three-for-two a-toothbrush discount-for-toothbrush)
+    (let* ((a-receipt (checks-out-articles-from a-teller a-cart)))
+      (is = expected-price (total-price a-receipt)))))
+
+(define-test "A three for two discount is granted for three of the right product."
+  :parent supermarket-receipt-testsuite
+  (let* ((a-catalog (make-instance 'fake-catalog))
+         (a-toothbrush (make-instance 'product
+                                      :name "toothbrush"
+                                      :unit 'each))
+         (price-for-toothbrush 1.00)
+         (number-of-toothbrushes 3.0)
+         (discount-for-toothbrush 10.0)
+         (expected-price 2.00)
+         (a-teller (make-instance 'teller
+                                  :catalog a-catalog))
+         (a-cart (make-instance 'shopping-cart)))
+    (add-product-to-catalog a-catalog a-toothbrush price-for-toothbrush)
+    (add-item-quantity a-cart a-toothbrush number-of-toothbrushes)
+    (add-special-offer a-teller 'three-for-two a-toothbrush discount-for-toothbrush)
+    (let* ((a-receipt (checks-out-articles-from a-teller a-cart)))
+      (is = expected-price (total-price a-receipt)))))
+
+(define-test "A two for amount discount is not granted for one of the right product."
+  :parent supermarket-receipt-testsuite
+  (let* ((a-catalog (make-instance 'fake-catalog))
+         (a-toothbrush (make-instance 'product
+                                      :name "toothbrush"
+                                      :unit 'each))
+         (price-for-toothbrush 1.00)
+         (number-of-toothbrushes 1.0)
+         (amount 1.50)
+         (expected-price 1.00)
+         (a-teller (make-instance 'teller
+                                  :catalog a-catalog))
+         (a-cart (make-instance 'shopping-cart)))
+    (add-product-to-catalog a-catalog a-toothbrush price-for-toothbrush)
+    (add-item-quantity a-cart a-toothbrush number-of-toothbrushes)
+    (add-special-offer a-teller 'two-for-amount a-toothbrush amount)
+    (let* ((a-receipt (checks-out-articles-from a-teller a-cart)))
+      (is = expected-price (total-price a-receipt)))))
+
+(define-test "A two for amount discount is granted for two of the right product."
+  :parent supermarket-receipt-testsuite
+  (let* ((a-catalog (make-instance 'fake-catalog))
+         (a-toothbrush (make-instance 'product
+                                      :name "toothbrush"
+                                      :unit 'each))
+         (price-for-toothbrush 1.00)
+         (number-of-toothbrushes 2.0)
+         (amount 1.50)
+         (expected-price 1.50)
+         (a-teller (make-instance 'teller
+                                  :catalog a-catalog))
+         (a-cart (make-instance 'shopping-cart)))
+    (add-product-to-catalog a-catalog a-toothbrush price-for-toothbrush)
+    (add-item-quantity a-cart a-toothbrush number-of-toothbrushes)
+    (add-special-offer a-teller 'two-for-amount a-toothbrush amount)
+    (let* ((a-receipt (checks-out-articles-from a-teller a-cart)))
+      (is = expected-price (total-price a-receipt)))))
+
+(define-test "A five for amount discount is not granted for four of the right product."
+  :parent supermarket-receipt-testsuite
+  (let* ((a-catalog (make-instance 'fake-catalog))
+         (a-toothbrush (make-instance 'product
+                                      :name "toothbrush"
+                                      :unit 'each))
+         (price-for-toothbrush 1.00)
+         (number-of-toothbrushes 4.0)
+         (amount 4.00)
+         (expected-price 4.00)
+         (a-teller (make-instance 'teller
+                                  :catalog a-catalog))
+         (a-cart (make-instance 'shopping-cart)))
+    (add-product-to-catalog a-catalog a-toothbrush price-for-toothbrush)
+    (add-item-quantity a-cart a-toothbrush number-of-toothbrushes)
+    (add-special-offer a-teller 'five-for-amount a-toothbrush amount)
+    (let* ((a-receipt (checks-out-articles-from a-teller a-cart)))
+      (is = expected-price (total-price a-receipt)))))
+
+(define-test "A five for amount discount is granted for five of the right product."
+  :parent supermarket-receipt-testsuite
+  (let* ((a-catalog (make-instance 'fake-catalog))
+         (a-toothbrush (make-instance 'product
+                                      :name "toothbrush"
+                                      :unit 'each))
+         (price-for-toothbrush 1.00)
+         (number-of-toothbrushes 5.0)
+         (amount 4.00)
+         (expected-price 4.00)
+         (a-teller (make-instance 'teller
+                                  :catalog a-catalog))
+         (a-cart (make-instance 'shopping-cart)))
+    (add-product-to-catalog a-catalog a-toothbrush price-for-toothbrush)
+    (add-item-quantity a-cart a-toothbrush number-of-toothbrushes)
+    (add-special-offer a-teller 'five-for-amount a-toothbrush amount)
+    (let* ((a-receipt (checks-out-articles-from a-teller a-cart)))
+      (is = expected-price (total-price a-receipt)))))
