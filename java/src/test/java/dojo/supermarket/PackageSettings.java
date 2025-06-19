0be37cb8d69d4c77175c714ecb628dbaa88da01d
emily <emily@bacheconsulting.com>
2025-06-19 15:39:10 +0200

    package settings present but not doing anything

--- java/src/test/java/dojo/supermarket/PackageSettings.java
+++ java/src/test/java/dojo/supermarket/PackageSettings.java
@@ -8,5 +8,6 @@ import org.approvaltests.reporters.JunitReporter;
  * Documentation: https://github.com/approvals/ApprovalTests.Java/blob/master/approvaltests/docs/Reporters.md
  */
 public class PackageSettings {
-    public static ApprovalFailureReporter UseReporter         = JunitReporter.INSTANCE;
+    // Uncomment this to stop it opening a diff window when an approval test fails
+    // public static ApprovalFailureReporter UseReporter = JunitReporter.INSTANCE;
 }
