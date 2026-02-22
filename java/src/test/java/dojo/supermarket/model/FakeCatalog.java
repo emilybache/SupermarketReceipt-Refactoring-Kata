c83d276cb40db4c26db3d2510ee4499b6afcb6e2
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2026-02-22 12:19:29 +0100

    using modern language features, mainly records

--- java/src/test/java/dojo/supermarket/model/FakeCatalog.java
+++ java/src/test/java/dojo/supermarket/model/FakeCatalog.java
@@ -4,17 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class FakeCatalog implements SupermarketCatalog {
-    private Map<String, Product> products = new HashMap<>();
-    private Map<String, Double> prices = new HashMap<>();
+
+    private final Map<String, Product> products = new HashMap<>();
+    private final Map<String, Double> prices = new HashMap<>();
 
     @Override
     public void addProduct(Product product, double price) {
-        this.products.put(product.getName(), product);
-        this.prices.put(product.getName(), price);
+        this.products.put(product.name(), product);
+        this.prices.put(product.name(), price);
     }
 
     @Override
     public double getUnitPrice(Product p) {
-        return this.prices.get(p.getName());
+        return this.prices.get(p.name());
     }
+
 }
