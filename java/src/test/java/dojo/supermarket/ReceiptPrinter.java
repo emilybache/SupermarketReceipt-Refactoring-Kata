719f96278b039db3a4fb6dc49117b7279521be32
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2026-02-23 20:27:52 +0100

    enum values named accordingly to java naming conventions

--- java/src/test/java/dojo/supermarket/ReceiptPrinter.java
+++ java/src/test/java/dojo/supermarket/ReceiptPrinter.java
@@ -74,7 +74,7 @@ public class ReceiptPrinter {
     }
 
     private static String presentQuantity(ReceiptItem item) {
-        return ProductUnit.Each.equals(item.getProduct().getUnit())
+        return ProductUnit.EACH.equals(item.getProduct().getUnit())
                 ? String.format("%x", (int)item.getQuantity())
                 : String.format(Locale.UK, "%.3f", item.getQuantity());
     }
