9b6b70202aa979b6b57254ef075db7f8de069b34
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2026-02-23 20:57:40 +0100

    ReceiptPrinterTest: Removed public modifier (class/methods) and made test variables private final

--- java/src/test/java/dojo/supermarket/ReceiptPrinterTest.java
+++ java/src/test/java/dojo/supermarket/ReceiptPrinterTest.java
@@ -6,33 +6,33 @@ import org.approvaltests.reporters.JunitReporter;
 import org.approvaltests.reporters.UseReporter;
 import org.junit.jupiter.api.Test;
 
-public class ReceiptPrinterTest {
+class ReceiptPrinterTest {
 
-    Product toothbrush = new Product("toothbrush", ProductUnit.Each);
-    Product apples = new Product("apples", ProductUnit.Kilo);
-    Receipt receipt = new Receipt();
+    private final Product toothbrush = new Product("toothbrush", ProductUnit.Each);
+    private final Product apples = new Product("apples", ProductUnit.Kilo);
+    private final Receipt receipt = new Receipt();
 
     @Test
-    public void oneLineItem() {
+    void oneLineItem() {
         receipt.addProduct(toothbrush, 1, 0.99, 0.99);
         Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
     }
 
     @Test
-    public void quantityTwo() {
+    void quantityTwo() {
         receipt.addProduct(toothbrush, 2, 0.99,0.99 * 2);
         Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
     }
 
     @Test
-    public void looseWeight() {
+    void looseWeight() {
         apples.setQuantityType(" KG");
         receipt.addProduct(apples, 2.3, 1.99,1.99 * 2.3);
         Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
     }
 
     @Test
-    public void total() {
+    void total() {
 
         receipt.addProduct(toothbrush, 1, 0.99, 2*0.99);
         receipt.addProduct(apples, 0.75, 1.99, 1.99*0.75);
@@ -40,13 +40,13 @@ public class ReceiptPrinterTest {
     }
 
     @Test
-    public void discounts() {
+    void discounts() {
         receipt.addDiscount(new Discount(apples, "3 for 2", -0.99));
         Approvals.verify(new ReceiptPrinter(40).printReceipt(receipt));
     }
 
     @Test
-    public void printWholeReceipt() {
+    void printWholeReceipt() {
         receipt.addProduct(toothbrush, 1, 0.99, 0.99);
         receipt.addProduct(toothbrush, 2, 0.99, 2*0.99);
         receipt.addProduct(apples, 0.75, 1.99, 1.99*0.75);
